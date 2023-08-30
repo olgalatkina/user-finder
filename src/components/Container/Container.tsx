2ad3da47209f72d74ca7651ee8d2922bfa212fcd
Olga Latkina <olga.latkina@gmail.com>
2023-08-30 16:00:02 +0700

    feat: add components Container, Header and ThemeSwitcher

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Container.module.scss';
+
+interface ContainerProps {
+  children: React.ReactNode,
+}
+
+export const Container = ({ children }: ContainerProps) => (
+  <div className={styles.container}>
+    {children}
+  </div>
+);
