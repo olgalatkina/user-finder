30eaaf53adb4731e140882dbc8303d580a212f2c
Olga Latkina <olga.latkina@gmail.com>
2023-08-31 08:06:05 +0700

    feat: add UserTitle

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,11 +1,25 @@
 import { UserStat } from 'components/UserStat';
 import { LocalGithubUser } from 'types';
 import styles from './UserCard.module.scss';
+import { UserTitle } from 'components/UserTitle';
 
 interface UserCardProps extends LocalGithubUser { }
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img
+      className={styles.avatar}
+      src={props.avatar}
+      alt={props.name}
+    />
+    <UserTitle
+      created={props.created}
+      login={props.login}
+      name={props.name}
+    />
+    <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio.'}
+    </p>
     <UserStat
       followers={props.followers}
       following={props.following}
